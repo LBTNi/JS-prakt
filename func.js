c9a5e4fcd42d4b5a1a6f07856470c1c1b699efd2
Nikita Lebed <Nikita.lebe78@gmail.com>
2021-10-27 00:39:24 +0300

    rel

--- func.js
+++ func.js
@@ -1,64 +1,64 @@
 function celciusToFahrenheit(){
     const celcius = document.getElementById('celcius').value;
-    const fahrenheit = (celcius * 9 / 5) + 32;
-    document.getElementById('fahrenheit').value = fahrenheit;
+    const fahrenheit=(celcius * 9 / 5) + 32;
+    document.getElementById('fahrenheit').value = parseFloat(fahrenheit);
 }
 function fahrenheitToCelcius(){
     const fahrenheit = document.getElementById('fahrenheit').value;
     const celcius = (fahrenheit - 32) * 5 / 9;
-    document.getElementById('celcius').value = celcius;
+    document.getElementById('celcius').value = parseFloat(celcius);
 }
 function kiloitgr(){
     const kilo = document.getElementById('kilo').value;
     const gr = kilo*1000;
-    document.getElementById('gr').value = gr;
+    document.getElementById('gr').value = parseFloat(gr);
 }
 function gritkilo(){
     const gr = document.getElementById('gr').value;
     const kilo = gr/1000;
-    document.getElementById('kilo').value = kilo;
+    document.getElementById('kilo').value = parseFloat(kilo);
 }
-function I(){
+function streigh(){
     const napr = document.getElementById('napr').value;
-    const R = document.getElementById('R').value;
-    const I = napr/R;
-    document.getElementById('I').value = I;
+    const sopr = document.getElementById('sopr').value;
+    const streigh = napr/sopr;
+    document.getElementById('streigh').value = parseFloat(streigh);
 }
-function V(){
-    const S = document.getElementById('S').value;
-    const t = document.getElementById('t').value;
-    const V = S/t;
-    document.getElementById('V').value = V;
+function speedv(){
+    const range = document.getElementById('range').value;
+    const time = document.getElementById('time').value;
+    const speedv = range/time;
+    document.getElementById('speedv').value = parseFloat(speedv);
 }
-function sinh(){
-    const s = document.getElementById('s').value;
-    const h = s/3600;
-    document.getElementById('h').value = h;
+function secondinhour(){
+    const second = document.getElementById('second').value;
+    const hour = second/3600;
+    document.getElementById('hour').value = parseFloat(hour);
 }
-function hins(){
-    const h = document.getElementById('h').value;
-    const s = h*3600;
-    document.getElementById('s').value = s;
+function hourinsecond(){
+    const hour = document.getElementById('hour').value;
+    const second = hour*3600;
+    document.getElementById('second').value = parseFloat(second);
 }
-function mlinl(){
-    const ml = document.getElementById('ml').value;
-    const l = ml/1000;
-    document.getElementById('l').value = l;
+function mlitrinlitr(){
+    const mlitr = document.getElementById('mlitr').value;
+    const litr = mlitr/1000;
+    document.getElementById('litr').value = parseFloat(litr);
 }
-function linml(){
-    const l = document.getElementById('l').value;
-    const ml = l*1000;
-    document.getElementById('ml').value = ml;
+function litrinmlitr(){
+    const litr = document.getElementById('litr').value;
+    const mlitr = litr*1000;
+    document.getElementById('mlitr').value = parseFloat(mlitr);
 }
-function p(){
-    const mp = document.getElementById('mp').value;
-    const vp = document.getElementById('vp').value;
-    const p = mp*vp;
-    document.getElementById('p').value = p;
+function impuls(){
+    const masspuls = document.getElementById('masspuls').value;
+    const speedpuls = document.getElementById('speedpuls').value;
+    const impuls = masspuls*speedpuls;
+    document.getElementById('impuls').value = parseFloat(impuls);
 }
-function l(){
-    const vl = document.getElementById('vl').value;
-    const T = document.getElementById('T').value;
-    const l = vl*T;
-    document.getElementById('l').value = l;
+function lymd(){
+    const speed = document.getElementById('speed').value;
+    const Takt = document.getElementById('Takt').value;
+    const lymd = speed*Takt;
+    document.getElementById('lymd').value = parseFloat(lymd);
 }
